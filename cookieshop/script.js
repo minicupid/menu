279f66dc0384681fcaf1e02abc293e8642da36dd
minicupid <jl16358@nyu.edu>
2025-03-01 23:47:00 -0500

    Update script.js
    music fix

--- cookieshop/script.js
+++ cookieshop/script.js
@@ -1,11 +1,13 @@
 var footer = document.querySelector(".footer");
 var audio = document.getElementById("crunch");
+var audio = document.getElementById("music");
 var display = true;
 
 function hideShow() {
     if (display) {
       footer.style.display = "none";
       crunch.play();
+        music.play();
 
     } else {
 
